44865ce3c69f9122d77c975d27cc3f0aa1686403
VemundFredriksen <vemund.fredriksen@hotmail.com>
2018-03-15 15:59:38 +0100

    The queries class can now prepare statements for adding apparat, øvelse and treningsøkt

--- JDBC/src/jdbc/Driver.java
+++ JDBC/src/jdbc/Driver.java
@@ -8,12 +8,21 @@ public class Driver {
 	private ResultSet myRs;
 	
 	public Driver() throws SQLException {
-		myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prosjekt?autoReconnect=true&useSSL=false", "root", "root");
+		myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prosjekt?autoReconnect=true&useSSL=false", "root", "");
 		myStmt = myConn.createStatement();
 	}
 	
 	public ResultSet executeQuery(String query) throws SQLException {
 		return this.myStmt.executeQuery(query);
+	}
+	
+	public void test() {
+		try {
+			this.myStmt.executeUpdate(Queries.INSERT_TRENINGSOKT(new Timestamp(117, 10,12,15,30,0,0), 40, "Fin økt", 4, 8));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
@@ -30,6 +39,7 @@ public class Driver {
 				System.out.println("ApparatID: " + myRs.getString("ApparatID") + " Navn: " + myRs.getString("Navn") + 
 				" Beskrivelse: " + myRs.getString("Beskrivelse"));
 			}
+			driver.test();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
