8eff3b090185f6c869e3e570a6bd86ccf3175e01
sigveks <sigveks.stud@ntnu.no>
2018-03-15 13:56:10 +0100

    #1 no har e laga me ei slik generell driver som kan ta inn queries og kalla ræi

--- JDBC/src/jdbc/Driver.java
+++ JDBC/src/jdbc/Driver.java
@@ -2,19 +2,28 @@ package jdbc;
 import java.sql.*;
 
 public class Driver {
+		
+	private Connection myConn;
+	private Statement myStmt;
+	private ResultSet myRs;
+	
+	public Driver() throws SQLException {
+		myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prosjekt?autoReconnect=true&useSSL=false", "root", "root");
+		myStmt = myConn.createStatement();
+	}
+	
+	public ResultSet executeQuery(String query) throws SQLException {
+		return this.myStmt.executeQuery(query);
+	}
 
 	public static void main(String[] args) {
 		
 		try {
-			// Connection
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prosjekt?autoReconnect=true&useSSL=false", "root", "root");
-			
-			//Statement
-			Statement myStmt = myConn.createStatement();
+			Driver driver = new Driver();
 			
 			//Queries
 			ResultSet myRs;
-			myRs = myStmt.executeQuery("select * from apparat");
+			myRs = driver.executeQuery("select * from apparat");
 			
 			//Process result
 			while(myRs.next()) {
