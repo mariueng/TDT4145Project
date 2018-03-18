1170a63105db8f4f6ffa76642b4ce21d37898837
VemundFredriksen <vemund.fredriksen@hotmail.com>
2018-03-18 12:45:25 +0100

    added Queries back

---
+++ JDBC/src/jdbc/Queries.java
@@ -0,0 +1,55 @@
+package jdbc;
+
+import java.sql.Timestamp;
+
+public final class Queries {
+	
+	/*
+	 * ================================ APPARATER ===================================
+	 */
+	
+	//Henter apparat gitt apparatId
+	public static final String GET_APPARAT_BY_ID(int id) {
+		return "SELECT * FROM apparat WHERE ApparatID LIKE " + id;
+	}
+	
+	//Setter inn nytt apparat med gitt navn og beskrivelse i tabellen. Id blir autogenerert i databasen
+	public static String INSERT_APPARAT(String navn, String beskrivelse) {
+		return "INSERT INTO apparat (Navn, Beskrivelse) VALUES (\"" + navn + "\", \"" + beskrivelse +"\")";
+	}
+	
+	/*
+	 * =============================== ØVELSER =====================================
+	 * Før enhver registrering av øvelse på NEW_OVELSE utføres først for å lage en entitet av superklassen.
+	 */
+	
+	//Lager en ny øvelse, dette er i prinsippet kun et heltall, men tabellen er brukt for å knytte ulike typer øvelser opp mot en superklasse
+	public static String NEW_OVELSE() {
+		return "INSERT INTO ovelse () VALUES ();";
+	}
+	
+	// Setter inn ny friøvelse gitt navn og beskrivelse. Denne spørringen forutsetter at en ny øvelse er laget på forhånd slik at
+	// friøvelsen kan knyttes opp mot denne hovedøvelsen
+	public static String INSERT_FRIOVELSE(String navn, String beskrivelse) {
+		return "INSERT INTO ovelsefriovelse (OvelseID, Navn, Beskrivelse) VALUES ((SELECT max(OvelseID) FROM Ovelse), \"" + navn + 
+				"\", \"" + beskrivelse + "\");";
+	}
+	
+	// Setter inn ny fastmontert øvelse gitt navn, antall kilo, antall sett og apparatId. Dersom apparatId ikke finnes vil SQL kaste en exception
+	// Denne spørringen forutsetter at en ny øvelse er laget på forhånd slik at den fastmonterte øvelsen kan knyttes mot superøvelsen
+	public static String INSERT_FASTMONTERT_OVELSE(String navn, double kg, int sett, int apparatId) {
+		return "INSERT INTO ovelsefastmontert (OvelseID, Navn, Kilogram, Sett, ApparatID) VALUES"
+				+ "((SELECT max(OvelseID) FROM Ovelse), \"" + navn + "\", \"" + kg + "\", \"" + sett + "\", " + "\" " + apparatId +"\");";
+	}
+	
+	/*
+	 * ================================= TRENINGSØKTER ===================================
+	 */
+	
+	//Setter inn en ny treningsøkt i databasen gitt tidspunkt, varighet, informasjom form og prestasjon
+	public static String INSERT_TRENINGSOKT(Timestamp datoTid, int varighetMin, String informasjon, int form, int prestasjon) {
+		return "INSERT INTO treningsoekt (DatoTidspunkt, Varighetminutter, Informasjon, Form, Prestasjon) VALUES"
+				+ "(\'" + datoTid + "\', " + varighetMin + ", " + "\"" + informasjon + "\", " + form + ", " + prestasjon +");";
+	}
+	
+}
